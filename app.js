6fedb42f6bf18a663b2ea8cea32566fe14c79116
Alan Kalkan <alan0613@live.fr>
2023-06-07 15:30:02 +0200

    adjusted cm/kg lb/in output

--- app.js
+++ app.js
@@ -3,17 +3,24 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const metric = document.getElementById("metric");
+  const imperial = document.getElementById("imperial");
   let firstUnit = document.getElementById("unitone");
   let secondUnit = document.getElementById("unittwo");
+  let switchFirstUnit = document.getElementsByClassName("unitone");
+  let switchSecondtUnit = document.getElementsByClassName("unittwo");
+
   metric.onclick = function () {
     firstUnit.innerText = "Height";
     secondUnit.innerText = "Weight";
+    switchFirstUnit[0].innerText = "cm";
+    switchSecondtUnit[0].innerText = "kg";
   };
 
-  const imperial = document.getElementById("imperial");
   imperial.onclick = function () {
     firstUnit.innerText = "Inches";
     secondUnit.innerText = "Pounds";
+    switchFirstUnit[0].innerText = "in";
+    switchSecondtUnit[0].innerText = "lb";
   };
 
   const heightInput = document.getElementById("height");
@@ -24,7 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (firstUnit.innerText == "Height" && secondUnit.innerText == "Weight") {
       const parsedHeight = parseFloat(heightInput.value);
       const parsedWeight = parseFloat(weightInput.value);
-
       const heightInMeters = parsedHeight / 100;
 
       let result =
@@ -33,18 +39,18 @@ document.addEventListener("DOMContentLoaded", function () {
       // console.log(result);
       const resultOutput = document.getElementById("result");
       resultOutput.innerText = result;
-    } else {
-        const parsedHeight = parseFloat(heightInput.value);
-        const parsedWeight = parseFloat(weightInput.value);
-  
-        const heightInMeters = parsedHeight / 100;
-  
-        let result =
-          Math.round((parsedWeight / (heightInMeters * heightInMeters)) * 10) /
-          10;
-          
-        const resultOutput = document.getElementById("result");
-        resultOutput.innerText = result;
+    } else if (
+      firstUnit.innerText == "Inches" &&
+      secondUnit.innerText == "Pounds"
+    ) {
+      const parsedHeight = parseFloat(heightInput.value / 39.37);
+      const parsedWeight = parseFloat(weightInput.value / 2.2046);
+
+      let result =
+        Math.round((parsedWeight / (parsedHeight * parsedHeight)) * 10) / 10;
+
+      const resultOutput = document.getElementById("result");
+      resultOutput.innerText = result;
     }
   });
 
