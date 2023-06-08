7d076699715a88e655f0007c37445f56ad25f1df
Alan Kalkan <alan0613@live.fr>
2023-06-08 10:56:47 +0200

    some more styling

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// IMC = Poids / Taille x Taille
-// IMC = (poids en livres / (hauteur en pouces * hauteur en pouces)) * 703
-
 document.addEventListener("DOMContentLoaded", function () {
   const metric = document.getElementById("metric");
   const imperial = document.getElementById("imperial");
@@ -8,12 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const secondUnit = document.getElementById("unittwo");
   const switchFirstUnit = document.getElementsByClassName("unitone");
   const switchSecondtUnit = document.getElementsByClassName("unittwo");
+  const heightInput = document.getElementById("height");
+  const weightInput = document.getElementById("weight");
 
   metric.onclick = function () {
     firstUnit.innerText = "Height";
     secondUnit.innerText = "Weight";
     switchFirstUnit[0].innerText = "cm";
     switchSecondtUnit[0].innerText = "kg";
+    heightInput.value = "";
+    weightInput.value = "";
   };
 
   imperial.onclick = function () {
@@ -21,18 +22,17 @@ document.addEventListener("DOMContentLoaded", function () {
     secondUnit.innerText = "Pounds";
     switchFirstUnit[0].innerText = "in";
     switchSecondtUnit[0].innerText = "lb";
+    heightInput.value = "";
+    weightInput.value = "";
   };
-
-  const heightInput = document.getElementById("height");
-  const weightInput = document.getElementById("weight");
 
   const calculateButton = document.getElementById("calculate");
   calculateButton.addEventListener("click", function () {
     if (
       firstUnit.innerText == "Height" &&
       secondUnit.innerText == "Weight" &&
-      !isNaN(firstUnit.innerText) &&
-      !isNaN(secondUnit.innerText)
+      !isNaN(heightInput.value) &&
+      !isNaN(weightInput.value)
     ) {
       const parsedHeight = parseFloat(heightInput.value);
       const parsedWeight = parseFloat(weightInput.value);
@@ -41,14 +41,14 @@ document.addEventListener("DOMContentLoaded", function () {
       let result =
         Math.round((parsedWeight / (heightInMeters * heightInMeters)) * 10) /
         10;
-      // console.log(result);
+
       const resultOutput = document.getElementById("result");
       resultOutput.innerText = result;
     } else if (
       firstUnit.innerText == "Inches" &&
       secondUnit.innerText == "Pounds" &&
-      !isNaN(firstUnit.innerText) &&
-      !isNaN(secondUnit.innerText)
+      !isNaN(heightInput.value) &&
+      !isNaN(weightInput.value)
     ) {
       const parsedHeight = parseFloat(heightInput.value / 39.37);
       const parsedWeight = parseFloat(weightInput.value / 2.2046);
@@ -60,15 +60,4 @@ document.addEventListener("DOMContentLoaded", function () {
       resultOutput.innerText = result;
     }
   });
-
-  //fin du domcontentloaded
 });
-
-// const bmcMetricCalc = (weigth, height) => {
-//     const resultBmc = Math.round((weigth / (height * height)) * 10) / 10;
-//     return resultBmc;}
-
-// const bmcImperialCalc = (pounds, feet, inches) => {
-//   const resultBmc = Math.round((weigth / (height * height)) * 10) / 10;
-//   return resultBmc;
-// }
